11fce9b6e7096cba2f4f035b2a3e443061a5243c
Quy Tran <quytrandev@gmail.com>
2021-02-02 08:43:12 +0700

    CRUD with ASP MVC/AJAX

---
+++ DBIO.cs
@@ -0,0 +1,69 @@
+﻿using DBProvider;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseIO
+{
+    public class DBIO
+    {
+        MyDB database = new MyDB();
+        
+        public Users GetUsers(string username, string password)
+        {
+            //without param
+            //string query = "SELECT * FROM Users WHERE id ='" + id + "' AND password ='" + password + "'";
+            //Users row;
+            //row = database.Database.SqlQuery<Users>(query).FirstOrDefault();
+            //return row;
+
+            //with params
+            //Users row;
+            //row = database.Database.SqlQuery<Users>("SELECT * FROM Users WHERE username = @username AND password = @password", 
+            //    new SqlParameter("@username", username),
+            //    new SqlParameter("@password",password)).FirstOrDefault();
+            //return row;
+
+            Users row;
+            row = database.Users.Where(q => q.username == username && q.password == password).FirstOrDefault();
+            return row;
+        }
+        
+        public int GetLatestID()
+        {
+            int number;
+            number = database.Users.Count();
+
+            return number;
+        }
+        public List<Users> GetUsersList()
+        {
+            return database.Users.OrderByDescending(q => q.ID).ToList();
+        }
+
+        public Users GetObject_Users(string id)
+        {
+            int deleteId = Int32.Parse(id);
+            return database.Users.Where(q => q.ID == deleteId).FirstOrDefault();
+        }
+        
+
+        //Add
+        public void AddObject<T>(T obj)
+        {
+            database.Set(obj.GetType()).Add(obj);
+        }
+        //Delete
+        public void DeleteObject<T>(T obj)
+        {
+            database.Set(obj.GetType()).Remove(obj);
+        }
+        public void Save()
+        {
+            database.SaveChanges();
+        }
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,168 @@
+﻿using DatabaseIO;
+using DBProvider;
+using MyASPMVCCSHARP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyASPMVCCSHARP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //query
+            DBIO db = new DBIO();
+            //DB object
+            //Users user = db.GetUsers("quytrandev","123456");
+
+            List<Users> list = db.GetUsersList();
+            return View(list);
+        }
+
+        [HttpPost]
+        public JsonResult AddUser(FormCollection data)
+        {
+            string username = data["username"];
+            string password = data["password"];
+
+            JsonResult json = new JsonResult();
+
+            if(String.IsNullOrEmpty(username) || String.IsNullOrEmpty(password))
+            {
+                json.Data = new
+                {
+                    status = "ERROR",
+                    message = "Data cannot be empty"
+                };
+            }
+            else
+            {
+                DBIO db = new DBIO();
+                Users user = new Users();
+
+                user.ID = db.GetLatestID() + 1;
+                user.username = username;
+                user.password = password;
+                db.AddObject(user);
+                db.Save();
+                json.Data = new
+                {
+                    status = "OK"
+                };
+            }
+
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult DeleteUser(FormCollection data)
+        {
+            string id = data["id"];
+     
+            JsonResult json = new JsonResult();
+
+            if (String.IsNullOrEmpty(id))
+            {
+                json.Data = new
+                {
+                    status = "ERROR",
+                    message = "Data cannot be empty"
+                };
+            }
+            else
+            {
+                DBIO db = new DBIO();
+                Users user = db.GetObject_Users(id);
+                
+                if(user == null)
+                {
+                    json.Data = new
+                    {
+                        status = "ERROR",
+                        message = "Data does not exist"
+                    };
+                }
+                else
+                {
+                    db.DeleteObject(user);
+                    db.Save();
+                    json.Data = new
+                    {
+                        status = "OK"
+                    };
+                }
+                
+            }
+
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult EditUser(FormCollection data)
+        {
+            string id = data["id"];
+            string username = data["username"].Trim();
+            string password = data["password"].Trim();
+
+            JsonResult json = new JsonResult();
+
+            if (String.IsNullOrEmpty(id) || String.IsNullOrEmpty(username))
+            {
+                json.Data = new
+                {
+                    status = "ERROR",
+                    message = "Data cannot be empty"
+                };
+            }
+            else
+            {
+                DBIO db = new DBIO();
+                Users user = db.GetObject_Users(id);
+
+                if (user == null)
+                {
+                    json.Data = new
+                    {
+                        status = "ERROR",
+                        message = "Data does not exist"
+                    };
+                }
+                else
+                {
+                    user.username = username;
+                    if(!String.IsNullOrEmpty(password))
+                    {
+                        user.password = password;
+                    }
+                    db.Save();
+                    json.Data = new
+                    {
+                        status = "OK"
+                    };
+                }
+
+            }
+
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult About(string id)
+        {
+            MyModel myModel = new MyModel();
+            myModel.name = "Yumi";
+            myModel.phone = "0385918111";
+            ViewBag.Message = "Your application description page." + id;
+
+            return View(myModel);
+        }
+
+        public ActionResult Contact(string name)
+        {
+            ViewBag.Message = "Your contact page." + name;
+
+            return View();
+        }
+    }
+}
